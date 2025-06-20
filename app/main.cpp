c699a00cd0046040816187371af3cf6ce1742cc9
Marcin Orszulak <marcinorszulak8@gmail.com>
2025-06-20 04:35:22 +0200

    Added TcpServerLib, to handle incoming tcp/ip telegrams from sensors

--- app/main.cpp
+++ app/main.cpp
@@ -1,1 +1,19 @@
-int main(){}
+#include <string>
+
+
+
+#include "TcpServer.h"
+
+int main()
+{
+    TcpServer server1;
+    server1.serverListen();
+
+    std::string command;
+
+    while (command != "exit")
+    {}
+    
+
+    return 0;
+}
